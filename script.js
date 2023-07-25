678817ee2f08537e97bedc367b550e9058efa71e
umarahgul <umarah.gul@gmail.com>
2023-07-25 15:06:51 +0500

    removed linter errors

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-
 function openNav() {
   document.getElementById('hamburger-menu').style.display = 'block';
   const div = document.querySelector('#top');
   div.classList.add('blur');
-  var userAgent = navigator.userAgent.toLowerCase();
+  const userAgent = navigator.userAgent.toLowerCase();
 
   // Check if "mobile" or "tablet" is NOT found in the user agent string
-  if (userAgent.indexOf("mobile") === -1 && userAgent.indexOf("tablet") === -1) {
-      div.classList.remove('blur');}
-
+  if (userAgent.indexOf('mobile') === -1 && userAgent.indexOf('tablet') === -1) {
+    div.classList.remove('blur');
+  }
 }
 function closeNav() {
   document.getElementById('hamburger-menu').style.display = 'none';
   const div = document.querySelector('#top');
   div.classList.remove('blur');
 }
-
-
-
 
 document.querySelector(' #hamburger-menu').addEventListener('click', openNav);
 
