b25e0f5c114872c931bdb0eaa27336cf5b931aca
umarahgul <umarah.gul@gmail.com>
2023-07-25 14:54:31 +0500

    set z-index:9999 for the languages tag which was a drop down menu in css and showed from behind the pop up menu

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+
 function openNav() {
   document.getElementById('hamburger-menu').style.display = 'block';
   const div = document.querySelector('#top');
   div.classList.add('blur');
+  var userAgent = navigator.userAgent.toLowerCase();
+
+  // Check if "mobile" or "tablet" is NOT found in the user agent string
+  if (userAgent.indexOf("mobile") === -1 && userAgent.indexOf("tablet") === -1) {
+      div.classList.remove('blur');}
+
 }
 function closeNav() {
   document.getElementById('hamburger-menu').style.display = 'none';
   const div = document.querySelector('#top');
   div.classList.remove('blur');
 }
+
+
+
 
 document.querySelector(' #hamburger-menu').addEventListener('click', openNav);
 
