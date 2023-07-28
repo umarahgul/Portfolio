5fb38198cf72badf5971e0cb5bd8abbb547a92c9
umarahgul <umarah.gul@gmail.com>
2023-07-28 17:15:59 +0500

    Removed linters errors

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function closeNav() {
 
 try {
   document.querySelector('#openMenuBtn').addEventListener('click', openNav);
-} catch { console.log("");}
+} catch { console.log(''); }
 document.querySelector('#closebtn').addEventListener('click', closeNav);
 
 // document.querySelector('').addEventListener('click',close_popup);
@@ -62,86 +62,81 @@ const arr_Mobile = [
     'link-to-source': '#',
   }];
 
+function Popup(name, descr, image, technology, l1, l2) {
+  document.getElementById('project-popup').style.display = 'block';
 
+  const parentElement = document.getElementById('project-popup');
+  const parent = document.createElement('section');
+  parent.classList.add('works', 'info_mobile');
 
-  function Popup(name, descr, image, technology, l1, l2) {
-    document.getElementById('project-popup').style.display = 'block';
-  
-    const parentElement = document.getElementById('project-popup');
-    const parent = document.createElement('section');
-    parent.classList.add('works', 'info_mobile');
-  
-    const div_close = document.createElement('div');
-    div_close.classList.add('div_close');
-  
-    const child1 = document.createElement('h1');
-    console.log(`value of name${name}`);
-    child1.textContent = name;
-    div_close.appendChild(child1);
-  
-    const close = document.createElement('img');
-    close.id = 'close_button';
-    // close.classList.add('close_button');
-    const x = document.createElement('div');
-    x.id = 'close';
-    div_close.appendChild(x);
-    close.src = 'images/close_button.png';
-    div_close.appendChild(close);
-    parent.appendChild(div_close);
-  
-    const child_img = document.createElement('img');
-    child_img.classList.add('right');
-    child_img.src = image;
-    parent.appendChild(child_img);
-  
-    const child_desc = document.createElement('p');
-    child_desc.textContent = descr;
-    child_desc.classList.add('pg2');
-    parent.appendChild(child_desc);
-  
-    const child_ul = document.createElement('ul');
-    child_ul.classList.add('stack');
-  
-    for (const tech of technology) {
-      const child_li = document.createElement('li');
-      child_li.textContent = tech;
-      child_li.classList.add('stack-elem');
-      child_ul.appendChild(child_li);
-    }
-    parent.appendChild(child_ul);
-  
-    const btns = document.createElement('div');
-    btns.classList.add('buttons');
-  
-    const buttonElement1 = document.createElement('button');
-    buttonElement1.classList.add('see-project');
-    buttonElement1.textContent = ('See Live');
-    const img_live = document.createElement('img');
-    img_live.src = 'images/see-live-icon.png';
-    buttonElement1.appendChild(img_live);
-    btns.appendChild(buttonElement1);
-  
-    const buttonElement2 = document.createElement('button');
-    buttonElement2.classList.add('see-project');
-    buttonElement2.textContent = ('See Source');
-  
-    const img_source = document.createElement('img');
-    img_source.src = 'images/Icon -GitHub.png';
-    buttonElement2.appendChild(img_source);
-    btns.appendChild(buttonElement2);
-    parent.appendChild(btns);
-  
-    parentElement.appendChild(parent);
+  const div_Close = document.createElement('div');
+  div_Close.classList.add('div_Close');
+
+  const child1 = document.createElement('h1');
+  child1.textContent = name;
+  div_Close.appendChild(child1);
+
+  const close = document.createElement('img');
+  close.id = 'close_button';
+  // close.classList.add('close_button');
+  const x = document.createElement('div');
+  x.id = 'close';
+  div_Close.appendChild(x);
+  close.src = 'images/close_button.png';
+  div_Close.appendChild(close);
+  parent.appendChild(div_Close);
+
+  const child_Img = document.createElement('img');
+  child_Img.classList.add('right');
+  child_Img.src = image;
+  parent.appendChild(child_Img);
+
+  const child_Desc = document.createElement('p');
+  child_Desc.textContent = descr;
+  child_Desc.classList.add('pg2');
+  parent.appendChild(child_Desc);
+
+  const child_ul = document.createElement('ul');
+  child_ul.classList.add('stack');
+
+  for (const tech of technology) {
+    const child_Li = document.createElement('li');
+    child_Li.textContent = tech;
+    child_Li.classList.add('stack-elem');
+    child_Ul.appendChild(child_Li);
   }
+  parent.appendChild(child_Ul);
 
+  const btns = document.createElement('div');
+  btns.classList.add('buttons');
 
+  const buttonElement1 = document.createElement('button');
+  buttonElement1.classList.add('see-project');
+  buttonElement1.textContent = ('See Live');
+  const img_live = document.createElement('img');
+  img_live.src = 'images/see-live-icon.png';
+  buttonElement1.appendChild(img_live);
+  btns.appendChild(buttonElement1);
+
+  const buttonElement2 = document.createElement('button');
+  buttonElement2.classList.add('see-project');
+  buttonElement2.textContent = ('See Source');
+
+  const img_source = document.createElement('img');
+  img_source.src = 'images/Icon -GitHub.png';
+  buttonElement2.appendChild(img_source);
+  btns.appendChild(buttonElement2);
+  parent.appendChild(btns);
+
+  parentElement.appendChild(parent);
+}
 
 /// //////////////////////////////////////////////MOBILE MOBILE ///////////////////////////////////////////////////////////////////////////////////////////////
 
 function populate_mainpage_mobile() {
   const parentElement = document.getElementById('parentElementId');
 
-  for (let x = 0; x < arr_Mobile.length; x+1) {
+  for (let x = 0; x < arr_Mobile.length; x + 1) {
     const parent = document.createElement('section');
     parent.classList.add('works', 'info_mobile');
     const child_Img = document.createElement('img');
@@ -184,13 +179,13 @@ function populate_mainpage_mobile() {
     buttonElement.addEventListener('click', (event) => {
       const butn = event.target;
       const projectIndex = butn.getAttribute('data-index');
-      const name_project = arr_Mobile[projectIndex].name;
-      const desc_project = arr_Mobile[projectIndex].description;
-      const image_project = arr_Mobile[projectIndex].image;
-      const tech_project = arr_Mobile[projectIndex].technology;
+      const name_Project = arr_Mobile[projectIndex].name;
+      const desc_Project = arr_Mobile[projectIndex].description;
+      const image_Project = arr_Mobile[projectIndex].image;
+      const tech_Project = arr_Mobile[projectIndex].technology;
       const link1_Project = arr_Mobile[projectIndex].link1;
       const link2_Project = arr_Mobile[projectIndex].link2;
-      Popup(name_project, desc_project, image_project, tech_project, link1_Project, link2_project);
+      Popup(name_project, desc_Project, image_Project, tech_Project, link1_Project, link2_Project);
     });
 
     parent.appendChild(info_Div);
@@ -201,7 +196,6 @@ function populate_mainpage_mobile() {
 document.addEventListener('DOMContentLoaded', populate_mainpage_mobile);
 
 /// //////////////////////////////////////////////////////////////POPOUP //////////////////////////////////////////////////////////////////////////////////////
-
 
 function close_popup() {
   document.getElementById('project-popup').style.display = 'none';
