68602e9d9cee0947ca31b038102c6c695304b12d
umarahgul <umarah.gul@gmail.com>
2023-07-29 04:15:26 +0500

    removed linter errors

--- script.js
+++ script.js
@@ -55,6 +55,71 @@ const arrMobile = [
     'link-live-version': '#',
     'link-to-source': '#',
   }];
+
+  function populateMainpage() {
+    const parentElement = document.getElementById('parentElementId');
+  
+    for (let x = 0; x < arrMobile.length; x += 1) {
+      const parent = document.createElement('section');
+      parent.classList.add('works', 'info_mobile');
+      parent.setAttribute('id', 'jump-to-works-section');
+      const childImg = document.createElement('img');
+      childImg.src = arrMobile[x].image;
+      parent.appendChild(childImg);
+  
+      const infoDiv = document.createElement('div');
+      infoDiv.classList.add('info');
+      parent.appendChild(infoDiv);
+      const child1 = document.createElement('h1');
+      child1.classList.add('tonic');
+      child1.textContent = arrMobile[x].name;
+  
+      infoDiv.appendChild(child1);
+  
+      const childUl = document.createElement('ul');
+      childUl.classList.add('stack');
+  
+      for (let y = 0; y < arrMobile[x].technology.length; y += 1) {
+        const childLi = document.createElement('li');
+        childLi.textContent = arrMobile[x].technology[y];
+        childLi.classList.add('stack-elem');
+        childUl.appendChild(childLi);
+      }
+  
+      infoDiv.appendChild(childUl);
+  
+      const childDesc = document.createElement('p');
+      childDesc.textContent = arrMobile[x].description;
+      childDesc.classList.add('pg2');
+      infoDiv.appendChild(childDesc);
+  
+      const buttonElement = document.createElement('button');
+      buttonElement.classList.add('see-project');
+  
+      buttonElement.textContent = ('See Project ');
+      infoDiv.appendChild(buttonElement);
+  
+      buttonElement.setAttribute('data-index', x);
+  
+      buttonElement.addEventListener('click', (event) => {
+        const butn = event.target;
+        const projectIndex = butn.getAttribute('data-index');
+        const nameProject = arrMobile[projectIndex].name;
+        const descProject = arrMobile[projectIndex].description;
+        const imageProject = arrMobile[projectIndex].image;
+        const techProject = arrMobile[projectIndex].technology;
+        const link1Project = arrMobile[projectIndex].link1;
+        const link2Project = arrMobile[projectIndex].link2;
+        Popup(nameProject, descProject, imageProject, techProject, link1Project, link2Project);
+      });
+  
+      parent.appendChild(infoDiv);
+      parentElement.appendChild(parent);
+    }
+  }
+  
+
+
 document.addEventListener('DOMContentLoaded', populateMainpage);
 
 function Popup(name, descr, image, technology, l1, l2) {
@@ -132,67 +197,6 @@ function closePopup() {
   document.getElementById('project-popup').style.display = 'none';
 }
 
-function populateMainpage() {
-  const parentElement = document.getElementById('parentElementId');
-
-  for (let x = 0; x < arrMobile.length; x += 1) {
-    const parent = document.createElement('section');
-    parent.classList.add('works', 'info_mobile');
-    parent.setAttribute('id', 'jump-to-works-section');
-    const childImg = document.createElement('img');
-    childImg.src = arrMobile[x].image;
-    parent.appendChild(childImg);
-
-    const infoDiv = document.createElement('div');
-    infoDiv.classList.add('info');
-    parent.appendChild(infoDiv);
-    const child1 = document.createElement('h1');
-    child1.classList.add('tonic');
-    child1.textContent = arrMobile[x].name;
-
-    infoDiv.appendChild(child1);
-
-    const childUl = document.createElement('ul');
-    childUl.classList.add('stack');
-
-    for (let y = 0; y < arrMobile[x].technology.length; y += 1) {
-      const childLi = document.createElement('li');
-      childLi.textContent = arrMobile[x].technology[y];
-      childLi.classList.add('stack-elem');
-      childUl.appendChild(childLi);
-    }
-
-    infoDiv.appendChild(childUl);
-
-    const childDesc = document.createElement('p');
-    childDesc.textContent = arrMobile[x].description;
-    childDesc.classList.add('pg2');
-    infoDiv.appendChild(childDesc);
-
-    const buttonElement = document.createElement('button');
-    buttonElement.classList.add('see-project');
-
-    buttonElement.textContent = ('See Project ');
-    infoDiv.appendChild(buttonElement);
-
-    buttonElement.setAttribute('data-index', x);
-
-    buttonElement.addEventListener('click', (event) => {
-      const butn = event.target;
-      const projectIndex = butn.getAttribute('data-index');
-      const nameProject = arrMobile[projectIndex].name;
-      const descProject = arrMobile[projectIndex].description;
-      const imageProject = arrMobile[projectIndex].image;
-      const techProject = arrMobile[projectIndex].technology;
-      const link1Project = arrMobile[projectIndex].link1;
-      const link2Project = arrMobile[projectIndex].link2;
-      Popup(nameProject, descProject, imageProject, techProject, link1Project, link2Project);
-    });
-
-    parent.appendChild(infoDiv);
-    parentElement.appendChild(parent);
-  }
-}
 
 // function validation(event) {
 //   const mail = document.getElementById('e-mail').value;
