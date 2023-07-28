930ad09b1c69afc51bfc77833dc4602e1cab379e
umarahgul <umarah.gul@gmail.com>
2023-07-29 04:16:44 +0500

    removed linter errors

--- script.js
+++ script.js
@@ -55,147 +55,148 @@ const arrMobile = [
     'link-live-version': '#',
     'link-to-source': '#',
   }];
-
-  function populateMainpage() {
-    const parentElement = document.getElementById('parentElementId');
+  function Popup(name, descr, image, technology, l1, l2) {
+    document.getElementById('project-popup').style.display = 'block';
   
-    for (let x = 0; x < arrMobile.length; x += 1) {
-      const parent = document.createElement('section');
-      parent.classList.add('works', 'info_mobile');
-      parent.setAttribute('id', 'jump-to-works-section');
-      const childImg = document.createElement('img');
-      childImg.src = arrMobile[x].image;
-      parent.appendChild(childImg);
+    const parentElement = document.getElementById('project-popup');
+    const parent = document.createElement('section');
+    parent.classList.add('works', 'info_mobile');
+    parent.setAttribute('id', 'popup-layout');
   
-      const infoDiv = document.createElement('div');
-      infoDiv.classList.add('info');
-      parent.appendChild(infoDiv);
-      const child1 = document.createElement('h1');
-      child1.classList.add('tonic');
-      child1.textContent = arrMobile[x].name;
+    const divClose = document.createElement('div');
+    divClose.classList.add('div_close');
   
-      infoDiv.appendChild(child1);
+    const child1 = document.createElement('h1');
+    child1.textContent = name;
+    divClose.appendChild(child1);
   
-      const childUl = document.createElement('ul');
-      childUl.classList.add('stack');
+    const close = document.createElement('img');
+    close.id = 'closeButton';
+    close.src = 'images/close_button.png';
+    divClose.appendChild(close);
+    parent.appendChild(divClose);
   
-      for (let y = 0; y < arrMobile[x].technology.length; y += 1) {
-        const childLi = document.createElement('li');
-        childLi.textContent = arrMobile[x].technology[y];
-        childLi.classList.add('stack-elem');
-        childUl.appendChild(childLi);
-      }
+    const childImg = document.createElement('img');
+    childImg.classList.add('right');
+    childImg.src = image;
+    parent.appendChild(childImg);
   
-      infoDiv.appendChild(childUl);
+    const childDesc = document.createElement('p');
+    childDesc.textContent = descr;
+    childDesc.classList.add('pg2');
+    parent.appendChild(childDesc);
   
-      const childDesc = document.createElement('p');
-      childDesc.textContent = arrMobile[x].description;
-      childDesc.classList.add('pg2');
-      infoDiv.appendChild(childDesc);
+    const childUl = document.createElement('ul');
+    childUl.classList.add('stack');
   
-      const buttonElement = document.createElement('button');
-      buttonElement.classList.add('see-project');
-  
-      buttonElement.textContent = ('See Project ');
-      infoDiv.appendChild(buttonElement);
-  
-      buttonElement.setAttribute('data-index', x);
-  
-      buttonElement.addEventListener('click', (event) => {
-        const butn = event.target;
-        const projectIndex = butn.getAttribute('data-index');
-        const nameProject = arrMobile[projectIndex].name;
-        const descProject = arrMobile[projectIndex].description;
-        const imageProject = arrMobile[projectIndex].image;
-        const techProject = arrMobile[projectIndex].technology;
-        const link1Project = arrMobile[projectIndex].link1;
-        const link2Project = arrMobile[projectIndex].link2;
-        Popup(nameProject, descProject, imageProject, techProject, link1Project, link2Project);
-      });
-  
-      parent.appendChild(infoDiv);
-      parentElement.appendChild(parent);
+    for (let y = 0; y < technology.length; y += 1) {
+      const childLi = document.createElement('li');
+      childLi.textContent = technology[y];
+      childLi.classList.add('stack-elem');
+      childUl.appendChild(childLi);
     }
+    parent.appendChild(childUl);
+  
+    const btns = document.createElement('div');
+    btns.classList.add('buttons1');
+  
+    const buttonElement1 = document.createElement('button');
+    buttonElement1.classList.add('see-project');
+    buttonElement1.innerHTML = `<a href ="${l1}"></a>`;
+    buttonElement1.textContent = ('See Live');
+  
+    const imgLive = document.createElement('img');
+    imgLive.src = 'images/see-live-icon.png';
+    buttonElement1.appendChild(imgLive);
+    btns.appendChild(buttonElement1);
+    l2 = 'https:\\google.com';
+    const buttonElement2 = document.createElement('button');
+    buttonElement2.classList.add('see-project');
+    buttonElement2.innerHTML = `<a href ="${l2}"></a>`;
+    buttonElement2.textContent = ('See Source');
+  
+    const imgSource = document.createElement('img');
+    imgSource.src = 'images/Icon -GitHub.png';
+    buttonElement2.appendChild(imgSource);
+    btns.appendChild(buttonElement2);
+    parent.appendChild(btns);
+  
+    parentElement.appendChild(parent);
+  
+    document.getElementById('closeButton').addEventListener('click', closePopup);
+  }
+  
+  function closePopup() {
+    document.getElementById('project-popup').style.display = 'none';
   }
   
 
+
+
+function populateMainpage() {
+  const parentElement = document.getElementById('parentElementId');
+
+  for (let x = 0; x < arrMobile.length; x += 1) {
+    const parent = document.createElement('section');
+    parent.classList.add('works', 'info_mobile');
+    parent.setAttribute('id', 'jump-to-works-section');
+    const childImg = document.createElement('img');
+    childImg.src = arrMobile[x].image;
+    parent.appendChild(childImg);
+
+    const infoDiv = document.createElement('div');
+    infoDiv.classList.add('info');
+    parent.appendChild(infoDiv);
+    const child1 = document.createElement('h1');
+    child1.classList.add('tonic');
+    child1.textContent = arrMobile[x].name;
+
+    infoDiv.appendChild(child1);
+
+    const childUl = document.createElement('ul');
+    childUl.classList.add('stack');
+
+    for (let y = 0; y < arrMobile[x].technology.length; y += 1) {
+      const childLi = document.createElement('li');
+      childLi.textContent = arrMobile[x].technology[y];
+      childLi.classList.add('stack-elem');
+      childUl.appendChild(childLi);
+    }
+
+    infoDiv.appendChild(childUl);
+
+    const childDesc = document.createElement('p');
+    childDesc.textContent = arrMobile[x].description;
+    childDesc.classList.add('pg2');
+    infoDiv.appendChild(childDesc);
+
+    const buttonElement = document.createElement('button');
+    buttonElement.classList.add('see-project');
+
+    buttonElement.textContent = ('See Project ');
+    infoDiv.appendChild(buttonElement);
+
+    buttonElement.setAttribute('data-index', x);
+
+    buttonElement.addEventListener('click', (event) => {
+      const butn = event.target;
+      const projectIndex = butn.getAttribute('data-index');
+      const nameProject = arrMobile[projectIndex].name;
+      const descProject = arrMobile[projectIndex].description;
+      const imageProject = arrMobile[projectIndex].image;
+      const techProject = arrMobile[projectIndex].technology;
+      const link1Project = arrMobile[projectIndex].link1;
+      const link2Project = arrMobile[projectIndex].link2;
+      Popup(nameProject, descProject, imageProject, techProject, link1Project, link2Project);
+    });
+
+    parent.appendChild(infoDiv);
+    parentElement.appendChild(parent);
+  }
+}
 
 document.addEventListener('DOMContentLoaded', populateMainpage);
 
-function Popup(name, descr, image, technology, l1, l2) {
-  document.getElementById('project-popup').style.display = 'block';
-
-  const parentElement = document.getElementById('project-popup');
-  const parent = document.createElement('section');
-  parent.classList.add('works', 'info_mobile');
-  parent.setAttribute('id', 'popup-layout');
-
-  const divClose = document.createElement('div');
-  divClose.classList.add('div_close');
-
-  const child1 = document.createElement('h1');
-  child1.textContent = name;
-  divClose.appendChild(child1);
-
-  const close = document.createElement('img');
-  close.id = 'closeButton';
-  close.src = 'images/close_button.png';
-  divClose.appendChild(close);
-  parent.appendChild(divClose);
-
-  const childImg = document.createElement('img');
-  childImg.classList.add('right');
-  childImg.src = image;
-  parent.appendChild(childImg);
-
-  const childDesc = document.createElement('p');
-  childDesc.textContent = descr;
-  childDesc.classList.add('pg2');
-  parent.appendChild(childDesc);
-
-  const childUl = document.createElement('ul');
-  childUl.classList.add('stack');
-
-  for (let y = 0; y < technology.length; y += 1) {
-    const childLi = document.createElement('li');
-    childLi.textContent = technology[y];
-    childLi.classList.add('stack-elem');
-    childUl.appendChild(childLi);
-  }
-  parent.appendChild(childUl);
-
-  const btns = document.createElement('div');
-  btns.classList.add('buttons1');
-
-  const buttonElement1 = document.createElement('button');
-  buttonElement1.classList.add('see-project');
-  buttonElement1.innerHTML = `<a href ="${l1}"></a>`;
-  buttonElement1.textContent = ('See Live');
-
-  const imgLive = document.createElement('img');
-  imgLive.src = 'images/see-live-icon.png';
-  buttonElement1.appendChild(imgLive);
-  btns.appendChild(buttonElement1);
-  l2 = 'https:\\google.com';
-  const buttonElement2 = document.createElement('button');
-  buttonElement2.classList.add('see-project');
-  buttonElement2.innerHTML = `<a href ="${l2}"></a>`;
-  buttonElement2.textContent = ('See Source');
-
-  const imgSource = document.createElement('img');
-  imgSource.src = 'images/Icon -GitHub.png';
-  buttonElement2.appendChild(imgSource);
-  btns.appendChild(buttonElement2);
-  parent.appendChild(btns);
-
-  parentElement.appendChild(parent);
-
-  document.getElementById('closeButton').addEventListener('click', closePopup);
-}
-
-function closePopup() {
-  document.getElementById('project-popup').style.display = 'none';
-}
 
 
 // function validation(event) {
